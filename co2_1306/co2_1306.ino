6e75e8df7fb6471439dff66d5e522df90164c505
nef <nefsation@gmail.com>
2019-03-12 11:37:27 -0300

    cambio de codigo

--- co2_1306/co2_1306.ino
+++ co2_1306/co2_1306.ino
@@ -1,13 +1,3 @@
-#include <MQ135.h>
-
-#include <OakOLED.h>
-
-#include <Adafruit_SSD1306.h>
-
-#include <Wire.h>
-
-//#include <SPI.h>
-
 /*
  * Carbon Dioxide Parts Per Million Meter
  * CO2PPM
@@ -22,7 +12,7 @@
 /*
  * Atmospheric CO2 Level..............400ppm
  * Average indoor co2.............350-450ppm
- * Maxiumum acceptable co2...........1200ppm
+ * Maxiumum acceptable co2...........1000ppm
  * Dangerous co2 levels.............>2000ppm
  */
 
@@ -34,23 +24,19 @@
 //---------------------------------------------------------------------------------------------------------------
 //                                                  LIBRARIES
 //---------------------------------------------------------------------------------------------------------------
-//#include <Wire.h>                             //I2C for OLED
+#include <Wire.h>                             //I2C for OLED
 #include <Adafruit_GFX.h>                     //grafix library for OLED
 #include <Adafruit_SSD1306.h>                 //OLED Driver
-//#include <LiquidCrystal.h>
-int backLight = 13;    // pin 13 will control the backlight
 
-//#include <SimpleDHT.h>
 //---------------------------------------------------------------------------------------------------------------
 //                                                   DEFINES
 //---------------------------------------------------------------------------------------------------------------
 #define anInput     A0                        //analog feed from MQ135
 #define digTrigger   2                        //digital feed from MQ135
 #define co2Zero     55                        //calibrated CO2 0 level
-//#define led          13                        //led on pin 9
-//#define OLED_RESET   4                        //OLED reset on lin 4
-//const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-//LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+#define led          9                        //led on pin 9
+#define OLED_RESET   4                        //OLED reset on lin 4
+
 
 //---------------------------------------------------------------------------------------------------------------
 //                                                LIBRARY CALL
@@ -65,13 +51,12 @@ void setup() {
   
   pinMode(anInput,INPUT);                     //MQ135 analog feed set for input
   pinMode(digTrigger,INPUT);                  //MQ135 digital feed set for input
-//  pinMode(led,OUTPUT);                        //led set for output
+  pinMode(led,OUTPUT);                        //led set for output
   Serial.begin(9600);                         //serial comms for debuging
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //begin display @ hex addy 0x3C
   display.display();                          //show buffer
   display.clearDisplay();                     //clear buffer
-  //lcd.begin(20, 4);             // columns, rows.  use 16,2 for a 16x2 LCD, etc.
-  //lcd.clear();                  // start with a blank screen
+  
 }
 //---------------------------------------------------------------------------------------------------------------
 //                                               MAIN LOOP
@@ -86,7 +71,7 @@ int zzz = 0;                                  //int for averaging
 int grafX = 0;                                //int for x value of graph
 
 
-  //display.clearDisplay();                     //clear display @ beginning of each loop
+  display.clearDisplay();                     //clear display @ beginning of each loop
 
   for (int x = 0;x<10;x++){                   //samplpe co2 10x over 2 seconds
     co2now[x]=analogRead(A0);
@@ -103,20 +88,18 @@ for (int x = 0;x<10;x++){                     //add samples together
 
   display.setTextSize(2);                     //set text size
   display.setTextColor(WHITE);                //set text color
-  lcd.setCursor(0,0);                     //set cursor
-  lcd.println("CO2 Level");               //print title
-  Serial.print("CO2: ");                       //skip a line
-  Serial.println(co2ppm);                       //skip a line
-
-  //lcd.print(co2ppm);                      //print co2 ppm
-  //lcd.print(" PPM");                      //print units
-  //grafX = map(co2ppm,0,1000,0,127);           //map value to screen width
-  //lcd.fillRect(0, 52, grafX, 10, WHITE);  //print graph 400min 1000max
-  //lcd.display();
-  }                          //show the buffer
-  //if(co2ppm>900){                             //if co2 ppm > 1000
-    //digitalWrite(led,HIGH);                   //turn on led
-  
-//  else{                                       //if not
-  //  digitalWrite(led,LOW);                    //turn off led
-//  }
+  display.setCursor(0,0);                     //set cursor
+  display.println("CO2 Level");               //print title
+  display.println(" ");                       //skip a line
+  display.print(co2ppm);                      //print co2 ppm
+  display.print(" PPM");                      //print units
+  grafX = map(co2ppm,0,1000,0,127);           //map value to screen width
+  display.fillRect(0, 52, grafX, 10, WHITE);  //print graph 400min 1000max
+  display.display();                          //show the buffer
+  if(co2ppm>999){                             //if co2 ppm > 1000
+    digitalWrite(led,HIGH);                   //turn on led
+  }
+  else{                                       //if not
+    digitalWrite(led,LOW);                    //turn off led
+  }
+}
